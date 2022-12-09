0472adfecc060d303e1c3fa507ee2f655057ffce
A B <abiten@As-MacBook-Pro.home>
2022-12-10 00:51:02 +0100

    initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import time
+
+
+def main():
+    start = time.time()
+
+    end = time.time()
+
+    print("Amount of time took: {}".format(end - start))
+
+
+if __name__ == "__main__":
+    main()
